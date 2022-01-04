f1981785762a4fd826db6238c31685831f9fa095
Karol <74067027+kAarol-k@users.noreply.github.com>
2022-01-04 20:41:49 +0100

    Initial commit

---
+++ src/SideBar.js
@@ -0,0 +1,24 @@
+import { BsPlus, BsFillLightningFill, BsGearFill } from 'react-icons/bs';
+import { FaFire, FaPoo } from 'react-icons/fa';
+
+
+const SideBar = () => {
+    return(
+      <div className="fixed top-0 left-0 h-screen  w-16 bg-gray-900 text-white flex flex-col shadow-lg">
+        <SideBarIcon icon={<FaFire size="28" />} />
+        <SideBarIcon icon={<BsPlus size="32" />} />
+        <SideBarIcon icon={<BsFillLightningFill size="20" />} />
+        <SideBarIcon icon={<FaPoo size="20" />} />
+      </div>
+
+    );
+
+};
+
+const SideBarIcon = ({icon}) =>(
+  <div className="sidebar-icon ">
+    {icon}
+  </div>
+
+);
+export default SideBar;
